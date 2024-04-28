ffd9fb293c99bce1481355a9d22fe27984c14c99
xarlein <alexvolkovdd@gmail.com>
2024-04-28 16:01:12 +0400

    feat(chat-xxx): initialize

--- index.js
+++ index.js
@@ -1,84 +1,46 @@
-import express from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
-import { Server } from 'socket.io';
-import sqlite3 from 'sqlite3';
-import { open } from 'sqlite';
-import { availableParallelism } from 'node:os';
-import cluster from 'node:cluster';
-import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
+import express from "express";
+import { createServer } from "node:http";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { Server } from "socket.io";
+import path from "path";
+import { configDotenv } from "dotenv";
 
-if (cluster.isPrimary) {
-  const numCPUs = availableParallelism();
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork({
-      PORT: 3000 + i
-    });
-  }
+configDotenv({});
 
-  setupPrimary();
-} else {
-  const db = await open({
-    filename: 'chat.db',
-    driver: sqlite3.Database
-  });
+const app = express();
+const server = createServer(app);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
-  await db.exec(`
-    CREATE TABLE IF NOT EXISTS messages (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      client_offset TEXT UNIQUE,
-      content TEXT
-    );
-  `);
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-  const app = express();
-  const server = createServer(app);
-  const io = new Server(server, {
-    connectionStateRecovery: {},
-    adapter: createAdapter()
-  });
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use("/css", express.static(path.join(__dirname, "css")));
 
-  const __dirname = dirname(fileURLToPath(import.meta.url));
+app.get("/", (_, res) => {
+  res.sendFile(join(__dirname, "index.html"));
+});
 
-  app.get('/', (req, res) => {
-    res.sendFile(join(__dirname, 'index.html'));
-  });
+io.on("connection", async (socket) => {
+  socket.on("chat message", async (msg) => {
+    io.emit("chat message", msg);
 
-  io.on('connection', async (socket) => {
-    socket.on('chat message', async (msg, clientOffset, callback) => {
-      let result;
-      try {
-        result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
-      } catch (e) {
-        if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
-          callback();
-        } else {
-          // nothing to do, just let the client retry
-        }
-        return;
+    setTimeout(() => {
+      if (msg.message === "ping") {
+        io.emit("chat message", { owner: "BOT", message: "pong" });
       }
-      io.emit('chat message', msg, result.lastID);
-      callback();
-    });
 
-    if (!socket.recovered) {
-      try {
-        await db.each('SELECT id, content FROM messages WHERE id > ?',
-          [socket.handshake.auth.serverOffset || 0],
-          (_err, row) => {
-            socket.emit('chat message', row.content, row.id);
-          }
-        )
-      } catch (e) {
-        // something went wrong
+      if (msg.message === "Привет") {
+        io.emit("chat message", { owner: "BOT", message: "Приветствую! Чем могу помочь?" });
       }
-    }
+    }, 1000);
   });
+});
 
-  const port = process.env.PORT;
+const port = process.env.PORT;
 
-  server.listen(port, () => {
-    console.log(`server running at http://localhost:${port}`);
-  });
-}
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
